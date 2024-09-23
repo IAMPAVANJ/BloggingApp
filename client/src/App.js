d05a195da63ecb9d566f027ff49d33a66bbf3c29
IAMPAVANJ <pavanjadhav45710@gmail.com>
2024-09-23 17:09:26 +0530

    removed unnecessary files and tailwind setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-red-300 text-5xl'>Hello Pavan</h1>
+    </div>
+  );
+}
+
+export default App;
